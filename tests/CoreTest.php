82cd6e7ece45a7aab37cd3d46376286c43397aed
Ronan Giron <ElGigi@users.noreply.github.com>
2018-12-28 16:38:45 +0100

    Mark tests as incomplete

--- tests/CoreTest.php
+++ tests/CoreTest.php
@@ -20,70 +20,84 @@ class CoreTest extends TestCase
     public function test__destruct()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testSerialize()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testGetCacheManager()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testGetDebug()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testGetComposer()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function test__construct()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testGetConfig()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testUnserialize()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testGetServiceContainer()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testSetLocale()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testGetLocale()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testGetPackage()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testGetDirectories()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 
     public function testOnTerminate()
     {
         // @todo
+        $this->markTestIncomplete('This test has not been implemented yet.');
     }
 }
