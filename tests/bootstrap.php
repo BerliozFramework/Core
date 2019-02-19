6b30b1a7f3a2736473d34532fbcecf7bab312d34
Ronan Giron <ElGigi@users.noreply.github.com>
2019-02-19 10:52:13 +0100

    Add test environment

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -9,6 +9,8 @@
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code, to the root.
  */
+putenv(sprintf('DOCUMENT_ROOT=%s', realpath(__DIR__ . '/_envTest/public')));
+
 $file = __DIR__ . '/../vendor/autoload.php';
 if (!file_exists($file)) {
     throw new RuntimeException('Install dependencies using composer to run the test suite.');
