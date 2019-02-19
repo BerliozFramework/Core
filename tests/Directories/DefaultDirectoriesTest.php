bb3be67718b46470ced5d849b0e834a66dc99270
Ronan Giron <ElGigi@users.noreply.github.com>
2019-02-19 10:53:08 +0100

    Tests of DefaultDirectories class

---
+++ tests/Directories/DefaultDirectoriesTest.php
@@ -0,0 +1,77 @@
+<?php
+/**
+ * This file is part of Berlioz framework.
+ *
+ * @license   https://opensource.org/licenses/MIT MIT License
+ * @copyright 2018 Ronan GIRON
+ * @author    Ronan GIRON <https://github.com/ElGigi>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code, to the root.
+ */
+
+namespace Berlioz\Core\Tests\Directories;
+
+use Berlioz\Core\Directories\DefaultDirectories;
+use PHPUnit\Framework\TestCase;
+
+class DefaultDirectoriesTest extends TestCase
+{
+    private function getAppDirectory(): string
+    {
+        return realpath(__DIR__ . '/../_envTest');
+    }
+
+    private function getDefaultDirectories(): DefaultDirectories
+    {
+        return new DefaultDirectories();
+    }
+
+    public function testGetWorkingDir()
+    {
+        $this->assertEquals(realpath($this->getAppDirectory() . DIRECTORY_SEPARATOR . 'public'),
+                            $this->getDefaultDirectories()->getWorkingDir());
+    }
+
+    public function testGetVarDir()
+    {
+        $this->assertEquals(realpath($this->getAppDirectory() . DIRECTORY_SEPARATOR . 'var'),
+                            $this->getDefaultDirectories()->getVarDir());
+    }
+
+    public function testGetDebugDir()
+    {
+        $this->assertEquals(realpath($this->getAppDirectory() . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . 'debug'),
+                            $this->getDefaultDirectories()->getDebugDir());
+    }
+
+    public function testGetCacheDir()
+    {
+        $this->assertEquals(realpath($this->getAppDirectory() . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . 'cache'),
+                            $this->getDefaultDirectories()->getCacheDir());
+    }
+
+    public function testGetLogDir()
+    {
+        $this->assertEquals(realpath($this->getAppDirectory() . DIRECTORY_SEPARATOR . 'var' . DIRECTORY_SEPARATOR . 'log'),
+                            $this->getDefaultDirectories()->getLogDir());
+    }
+
+    public function testGetConfigDir()
+    {
+        $this->assertEquals(realpath($this->getAppDirectory() . DIRECTORY_SEPARATOR . 'config'),
+                            $this->getDefaultDirectories()->getConfigDir());
+    }
+
+    public function testGetAppDir()
+    {
+        $this->assertEquals(realpath($this->getAppDirectory()),
+                            $this->getDefaultDirectories()->getAppDir());
+    }
+
+    public function testGetVendorDir()
+    {
+        $this->assertEquals(realpath($this->getAppDirectory() . DIRECTORY_SEPARATOR . 'vendor'),
+                            $this->getDefaultDirectories()->getVendorDir());
+    }
+}
